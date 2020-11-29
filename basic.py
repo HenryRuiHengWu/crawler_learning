5c9111069d90ebfc8d809e79f23e0e263f1a226e
HenryRuiHengWu <21910069@zju.edu.cn>
2020-11-29 22:15:03 +0800

    new

--- basic.py
+++ basic.py
@@ -2,20 +2,21 @@
 # -*- encoding: utf-8 -*-
 '''
 @File    :   basic.py
-@Time    :   2020/11/03 21:16:17
+@Time    :   2020/11/29 22:05:31
 @Author  :   HenryWu 
 @Version :   1.0
 @Contact :   21910069@zju.edu.cn
 '''
 # Start typing your code from here
 
-
 import math
 
+
 def quadratic(a, b, c):
-    s1=(-b+math.sqrt(b**2-4*a*c))/(2*a)
-    s2=(-b-math.sqrt(b**2-4*a*c))/(2*a)
-    return s1,s2
+    s1 = (-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+    s2 = (-b - math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+    return s1, s2
+
 
 # 测试:
 print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
